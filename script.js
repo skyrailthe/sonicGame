ff34d4dc3f6444bbdad8cd7493499a87677afd34
skyrailthe <skyrail1994@gmail.com>
2020-09-11 21:50:25 +0300

    исправлена фотографий

--- script.js
+++ script.js
@@ -2,179 +2,179 @@ let data = {
     "articles": [{
         "id": 1,
         "title": "Соник",
-        "image": "https://static.wikia.nocookie.net/sonic/images/1/11/TSR_Sonic_the_Hedgehog.png/revision/latest/scale-to-width-down/319?cb=20191012210137&path-prefix=ru"
+        "image": "https://upload.wikimedia.org/wikipedia/ru/thumb/d/d0/Sonic.png/181px-Sonic.png"
     }, {
         "id": 2,
         "title": "Теилс",
-        "image": "https://static.wikia.nocookie.net/sonic/images/0/00/SLW_Miles_Prower.png/revision/latest/scale-to-width-down/350?cb=20190110172029&path-prefix=ru"
+        "image": "https://upload.wikimedia.org/wikipedia/ru/thumb/7/76/Unleashed_tails.png/274px-Unleashed_tails.png"
     }, {
         "id": 3,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/0/0a/TSR_Knuckles_the_Echidna.png/revision/latest/scale-to-width-down/242?cb=20190406123457&path-prefix=ru"
+        "image": "https://upload.wikimedia.org/wikipedia/ru/0/08/Knuckles06.png"
     }, {
         "id": 4,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/1/18/TSR_Amy.png/revision/latest/scale-to-width-down/338?cb=20190105173959&path-prefix=ru"
+        "image": "https://upload.wikimedia.org/wikipedia/ru/d/d1/18_Amyrose.jpg"
     },
     {
         "id": 5,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/1/14/TSR_Doctor_Eggman.png/revision/latest/scale-to-width-down/350?cb=20200225225348&path-prefix=ru"
+        "image": "https://upload.wikimedia.org/wikipedia/ru/thumb/c/c5/25_Cream.png/274px-25_Cream.png"
     },
     {
         "id": 6,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/d/d9/TSR_Metal_Sonic.png/revision/latest/scale-to-width-down/211?cb=20191011210827&path-prefix=ru"
+        "image": "https://upload.wikimedia.org/wikipedia/ru/thumb/d/db/Unleashed_eggman.png/243px-Unleashed_eggman.png"
     },
     {
         "id": 7,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/2/29/TSR_Shadow_the_Hedgehog.png/revision/latest/scale-to-width-down/306?cb=20190322131758&path-prefix=ru"
+        "image": "https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%81%D0%B6_%D0%A8%D1%8D%D0%B4%D0%BE%D1%83.png"
     },
     {
         "id": 8,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/f/fd/TSR_Rouge_the_Bat.png/revision/latest/scale-to-width-down/209?cb=20191031154959&path-prefix=ru"
+        "image": "https://upload.wikimedia.org/wikipedia/ru/c/cb/RougeTheBat.png"
     },
     {
         "id": 9,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/5/54/TSR_E-123_Omega.png/revision/latest/scale-to-width-down/350?cb=20200321143507&path-prefix=ru"
+        "image": "https://upload.wikimedia.org/wikipedia/ru/thumb/c/c1/Big_the_Cat.png/274px-Big_the_Cat.png"
     },
     {
         "id": 10,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/e/ee/SF-SB_Espio_the_Chameleon.png/revision/latest/scale-to-width-down/350?cb=20190324143547&path-prefix=ru"
+        "image": "https://upload.wikimedia.org/wikipedia/ru/thumb/0/07/Vector.gif/200px-Vector.gif"
     },
     {
         "id": 11,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/1/11/TSR_Vector_the_Crocodile.png/revision/latest/scale-to-width-down/331?cb=20200721130646&path-prefix=ru"
+        "image": "https://upload.wikimedia.org/wikipedia/ru/thumb/e/e7/01._%D0%AD%D1%81%D0%BF%D0%B8%D0%BE.PNG/140px-01._%D0%AD%D1%81%D0%BF%D0%B8%D0%BE.PNG"
     },
     {
         "id": 12,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/1/1f/SH_Charmy_Bee.png/revision/latest/scale-to-width-down/337?cb=20190325170516&path-prefix=ru"
+        "image": "https://upload.wikimedia.org/wikipedia/ru/thumb/a/a6/04_%D0%A7%D0%B0%D1%80%D0%BC%D0%B8.PNG/150px-04_%D0%A7%D0%B0%D1%80%D0%BC%D0%B8.PNG"
     },
     {
         "id": 13,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/c/cf/TSR_Big_the_Cat.png/revision/latest/scale-to-width-down/350?cb=20190427105152&path-prefix=ru"
+        "image": "https://upload.wikimedia.org/wikipedia/ru/a/a2/Silver_The_Hedgehog_2006.png"
     },
     {
         "id": 14,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/8/8f/SF%28SB%29_Froggy.png/revision/latest/scale-to-width-down/350?cb=20190528112516&path-prefix=ru"
+        "image": "https://upload.wikimedia.org/wikipedia/ru/thumb/b/b0/24_Blaze.png/204px-24_Blaze.png"
     },
     {
         "id": 15,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/6/6a/SF-SB_Cream_the_Rabbit.png/revision/latest/scale-to-width-down/350?cb=20190328151526&path-prefix=ru"
+        "image": "https://upload.wikimedia.org/wikipedia/ru/b/bc/Sticks-the-Badger.png"
     },
     {
         "id": 16,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/8/85/SFSB_Tikal_the_Echidna.png/revision/latest/scale-to-width-down/275?cb=20200613113631&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/legacy_e_123_omega_render_by_nibroc_rock-db2r0uj.png?w=300&h=300"
     },
     {
         "id": 17,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/6/6b/SFR_Jet_the_Hawk.png/revision/latest/scale-to-width-down/269?cb=20190811084138&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/marine_the_raccoon_legacy_render_by_nibroc_rock-daphhmk.png?w=300&h=300"
     },
     {
         "id": 18,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/0/0a/SFR_Storm_the_Albatross.png/revision/latest/scale-to-width-down/350?cb=20191231135248&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/mighty_the_armadillo_2018_render_by_nibroc_rock-dc8dbcn.png?w=300&h=300"
     },
     {
         "id": 19,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/a/a4/SFR_Wave_the_Swallow.png/revision/latest/scale-to-width-down/350?cb=20200102173245&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/ray_the_flying_squirrel_mania_version_render_by_nibroc_rock-dc8n02o.png?w=300&h=300"
     },
     {
         "id": 20,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/3/37/SA_E-102.png/revision/latest/scale-to-width-down/350?cb=20190624145229&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/legacy_chaos_0_render__updated__by_nibroc_rock-db1n1b1.png?w=300&h=300"
     },
     {
         "id": 21,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/1/15/SA_E-101.png/revision/latest/scale-to-width-down/350?cb=20190624145228&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/tikal_2018_render_by_nibroc_rock-dcmgf2g.png?w=300&h=300"
     },
     {
         "id": 22,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/a/ab/SA_Pachacamac.png/revision/latest/scale-to-width-down/350?cb=20190401170641&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/jet_the_hawk_2018_render_by_nibroc_rock-dc55ddo.png?w=300&h=300"
     },
     {
         "id": 23,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/0/0d/SC_%D0%9E%D1%80%D0%B1%D0%BE%D1%82.png/revision/latest/scale-to-width-down/300?cb=20200210003013&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/wave_the_swallow_legacy__updated__render_by_nibroc_rock-db2lgq5.png?w=300&h=300"
     },
     {
         "id": 24,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/f/f5/SB_Sticks_the_Badger.png/revision/latest/scale-to-width-down/311?cb=20200320153835&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/storm_the_albatross_legacy__updated__render_by_nibroc_rock-db2lgt5.png?w=300&h=300"
     },
     {
         "id": 25,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/6/6d/SLW_%D0%97%D0%B0%D0%B2%D0%BE%D0%BA.png/revision/latest/scale-to-width-down/350?cb=20200209013040&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/legacy_mephiles_the_dark_render_auraless_by_nibroc_rock-db2vnqy.png?w=300&h=300"
     },
     {
         "id": 26,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/0/06/SLW_%D0%97%D0%B0%D0%B7%D0%B7.png/revision/latest/scale-to-width-down/350?cb=20190608131831&path-prefix=ru"
+        "image": "https://i0.wp.com/pre03.deviantart.net/0837/th/pre/i/2015/331/7/6/e_102_gamma_render__adventure_3_3_by_nibroc_rock-d9i8948.png"
     },
     {
         "id": 27,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/9/95/SF_%28SB%29_Zeena.png/revision/latest/scale-to-width-down/229?cb=20200529140137&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/metal_sonic_2018_render_by_nibroc_rock-dcnm0mp.png?w=300&h=300"
     },
     {
         "id": 28,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/f/f7/SA_ZERO.png/revision/latest/scale-to-width-down/350?cb=20190624145231&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/infinite_smash4_main_render_by_nibroc_rock-dbn86c9.png?w=300&h=300"
     },
     {
         "id": 29,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/9/98/SLW_%D0%97%D0%BE%D0%BC%D0%BE%D0%BC.png/revision/latest/scale-to-width-down/350?cb=20200213072730&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/e_101_beta_mk2_render_by_nibroc_rock-daoum0d.png?w=300&h=300"
     },
     {
         "id": 30,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/d/da/SF_Infinite.png/revision/latest/scale-to-width-down/271?cb=20190623094440&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/doom_s_eye_render_by_nibroc_rock-dbpdzov.png?w=301&h=300"
     },
     {
         "id": 31,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/5/5e/TSR_%D0%91%D0%BB%D1%8D%D0%B9%D0%B7.png/revision/latest/scale-to-width-down/272?cb=20190611164328&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2019/04/zomom_of_the_deadly_six_render__legacy_render__by_nibroc_rock_dbehwd9-pre.png?w=360"
     },
     {
         "id": 32,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/2/25/SC_Cubot.png/revision/latest/scale-to-width-down/350?cb=20200319164112&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/orbot_legacy_render_by_nibroc_rock-dappzpm.png?w=300&h=300"
     },
     {
         "id": 33,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/5/5f/SU_Chip.png/revision/latest/scale-to-width-down/207?cb=20191028182553&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/cubot_legacy_render_by_nibroc_rock-dapq0h3.png?w=300&h=300"
     },
     {
         "id": 34,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/a/a0/SH_Cheese.png/revision/latest/scale-to-width-down/310?cb=20190722122833&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/maria_render_by_nibroc_rock-dboy5g3.png?w=300&h=300"
     },
     {
         "id": 35,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/5/52/SF_Chaos.png/revision/latest/scale-to-width-down/350?cb=20190430121744&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/legacy_vanilla_the_rabbit_render_by_nibroc_rock-db1ijyk.png?w=400&h=400"
     },
     {
         "id": 36,
         "title": "Lorem Ipsum is simply dummy",
-        "image": "https://static.wikia.nocookie.net/sonic/images/7/7f/TSR_Dark_Chao.png/revision/latest/scale-to-width-down/179?cb=20191001195936&path-prefix=ru"
+        "image": "https://sonicworldfangame.files.wordpress.com/2014/08/chocola_updated_render__legacy__by_nibroc_rock-db1jd9c.png?w=200&h=200"
     },]
 },
     image = document.createElement('img'),
