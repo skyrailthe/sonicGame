9aa95c22f0a7475adffb0cd55b34cc5a13fd6a39
skyrailthe <skyrail1994@gmail.com>
2020-09-11 20:29:12 +0300

    добавил картинки соника data.json

---
+++ script.js
@@ -0,0 +1,194 @@
+let data = {
+    "articles": [{
+        "id": 1,
+        "title": "Соник",
+        "image": "https://static.wikia.nocookie.net/sonic/images/1/11/TSR_Sonic_the_Hedgehog.png/revision/latest/scale-to-width-down/319?cb=20191012210137&path-prefix=ru"
+    }, {
+        "id": 2,
+        "title": "Теилс",
+        "image": "https://static.wikia.nocookie.net/sonic/images/0/00/SLW_Miles_Prower.png/revision/latest/scale-to-width-down/350?cb=20190110172029&path-prefix=ru"
+    }, {
+        "id": 3,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/0/0a/TSR_Knuckles_the_Echidna.png/revision/latest/scale-to-width-down/242?cb=20190406123457&path-prefix=ru"
+    }, {
+        "id": 4,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/1/18/TSR_Amy.png/revision/latest/scale-to-width-down/338?cb=20190105173959&path-prefix=ru"
+    },
+    {
+        "id": 5,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/1/14/TSR_Doctor_Eggman.png/revision/latest/scale-to-width-down/350?cb=20200225225348&path-prefix=ru"
+    },
+    {
+        "id": 6,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/d/d9/TSR_Metal_Sonic.png/revision/latest/scale-to-width-down/211?cb=20191011210827&path-prefix=ru"
+    },
+    {
+        "id": 7,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/2/29/TSR_Shadow_the_Hedgehog.png/revision/latest/scale-to-width-down/306?cb=20190322131758&path-prefix=ru"
+    },
+    {
+        "id": 8,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/f/fd/TSR_Rouge_the_Bat.png/revision/latest/scale-to-width-down/209?cb=20191031154959&path-prefix=ru"
+    },
+    {
+        "id": 9,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/5/54/TSR_E-123_Omega.png/revision/latest/scale-to-width-down/350?cb=20200321143507&path-prefix=ru"
+    },
+    {
+        "id": 10,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/e/ee/SF-SB_Espio_the_Chameleon.png/revision/latest/scale-to-width-down/350?cb=20190324143547&path-prefix=ru"
+    },
+    {
+        "id": 11,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/1/11/TSR_Vector_the_Crocodile.png/revision/latest/scale-to-width-down/331?cb=20200721130646&path-prefix=ru"
+    },
+    {
+        "id": 12,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/1/1f/SH_Charmy_Bee.png/revision/latest/scale-to-width-down/337?cb=20190325170516&path-prefix=ru"
+    },
+    {
+        "id": 13,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/c/cf/TSR_Big_the_Cat.png/revision/latest/scale-to-width-down/350?cb=20190427105152&path-prefix=ru"
+    },
+    {
+        "id": 14,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/8/8f/SF%28SB%29_Froggy.png/revision/latest/scale-to-width-down/350?cb=20190528112516&path-prefix=ru"
+    },
+    {
+        "id": 15,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/6/6a/SF-SB_Cream_the_Rabbit.png/revision/latest/scale-to-width-down/350?cb=20190328151526&path-prefix=ru"
+    },
+    {
+        "id": 16,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/8/85/SFSB_Tikal_the_Echidna.png/revision/latest/scale-to-width-down/275?cb=20200613113631&path-prefix=ru"
+    },
+    {
+        "id": 17,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/6/6b/SFR_Jet_the_Hawk.png/revision/latest/scale-to-width-down/269?cb=20190811084138&path-prefix=ru"
+    },
+    {
+        "id": 18,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/0/0a/SFR_Storm_the_Albatross.png/revision/latest/scale-to-width-down/350?cb=20191231135248&path-prefix=ru"
+    },
+    {
+        "id": 19,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/a/a4/SFR_Wave_the_Swallow.png/revision/latest/scale-to-width-down/350?cb=20200102173245&path-prefix=ru"
+    },
+    {
+        "id": 20,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/3/37/SA_E-102.png/revision/latest/scale-to-width-down/350?cb=20190624145229&path-prefix=ru"
+    },
+    {
+        "id": 21,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/1/15/SA_E-101.png/revision/latest/scale-to-width-down/350?cb=20190624145228&path-prefix=ru"
+    },
+    {
+        "id": 22,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/a/ab/SA_Pachacamac.png/revision/latest/scale-to-width-down/350?cb=20190401170641&path-prefix=ru"
+    },
+    {
+        "id": 23,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/0/0d/SC_%D0%9E%D1%80%D0%B1%D0%BE%D1%82.png/revision/latest/scale-to-width-down/300?cb=20200210003013&path-prefix=ru"
+    },
+    {
+        "id": 24,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/f/f5/SB_Sticks_the_Badger.png/revision/latest/scale-to-width-down/311?cb=20200320153835&path-prefix=ru"
+    },
+    {
+        "id": 25,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/6/6d/SLW_%D0%97%D0%B0%D0%B2%D0%BE%D0%BA.png/revision/latest/scale-to-width-down/350?cb=20200209013040&path-prefix=ru"
+    },
+    {
+        "id": 26,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/0/06/SLW_%D0%97%D0%B0%D0%B7%D0%B7.png/revision/latest/scale-to-width-down/350?cb=20190608131831&path-prefix=ru"
+    },
+    {
+        "id": 27,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/9/95/SF_%28SB%29_Zeena.png/revision/latest/scale-to-width-down/229?cb=20200529140137&path-prefix=ru"
+    },
+    {
+        "id": 28,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/f/f7/SA_ZERO.png/revision/latest/scale-to-width-down/350?cb=20190624145231&path-prefix=ru"
+    },
+    {
+        "id": 29,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/9/98/SLW_%D0%97%D0%BE%D0%BC%D0%BE%D0%BC.png/revision/latest/scale-to-width-down/350?cb=20200213072730&path-prefix=ru"
+    },
+    {
+        "id": 30,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/d/da/SF_Infinite.png/revision/latest/scale-to-width-down/271?cb=20190623094440&path-prefix=ru"
+    },
+    {
+        "id": 31,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/5/5e/TSR_%D0%91%D0%BB%D1%8D%D0%B9%D0%B7.png/revision/latest/scale-to-width-down/272?cb=20190611164328&path-prefix=ru"
+    },
+    {
+        "id": 32,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/2/25/SC_Cubot.png/revision/latest/scale-to-width-down/350?cb=20200319164112&path-prefix=ru"
+    },
+    {
+        "id": 33,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/5/5f/SU_Chip.png/revision/latest/scale-to-width-down/207?cb=20191028182553&path-prefix=ru"
+    },
+    {
+        "id": 34,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/a/a0/SH_Cheese.png/revision/latest/scale-to-width-down/310?cb=20190722122833&path-prefix=ru"
+    },
+    {
+        "id": 35,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/5/52/SF_Chaos.png/revision/latest/scale-to-width-down/350?cb=20190430121744&path-prefix=ru"
+    },
+    {
+        "id": 36,
+        "title": "Lorem Ipsum is simply dummy",
+        "image": "https://static.wikia.nocookie.net/sonic/images/7/7f/TSR_Dark_Chao.png/revision/latest/scale-to-width-down/179?cb=20191001195936&path-prefix=ru"
+    },]
+},
+    image = document.createElement('img'),
+    receivingElement,
+    idPrefix = 'image_url_',
+    imageClone,
+    title;
+
+data.articles.forEach(function (obj, index) {
+    receivingElement = document.getElementById(idPrefix + (index + 1));
+    if (receivingElement) {
+        imageClone = image.cloneNode();
+        imageClone.src = obj.image;
+        receivingElement.appendChild(imageClone);
+    }
+
+});
